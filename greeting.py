ee0d30cdf371cae7bf77e8f0f499ab4be9d6cc52
nithyanands <39807199+nithyanands@users.noreply.github.com>
2026-06-17 09:27:14 +0100

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -9,4 +9,6 @@ def helloall(): # different method name
  name = request.args.get('name')#retrieve GET parameters
  return("Hello {}!".format(name))#Python’s string.format
 if __name__ == "__main__":
- app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
+ #app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Not used as GCP not provides DOMAIN ADDRESS, secure connection requires domain
+ app.run(host='0.0.0.0',port='8080') # swap comments if you have a cert
+#Run the flask app at port 8080
